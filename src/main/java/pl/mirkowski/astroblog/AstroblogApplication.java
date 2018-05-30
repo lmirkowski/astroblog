64defc1ae87c462e6f73b3a89a6f0f381e60ed92
l.mirkowski <mirkowskilukasz@gmail.com>
2018-05-30 17:12:50 +0200

    project schema

---
+++ src/main/java/pl/mirkowski/astroblog/AstroblogApplication.java
@@ -0,0 +1,12 @@
+package pl.mirkowski.astroblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstroblogApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/mirkowski/astroblog/AstroblogApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mirkowski.astroblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AstroblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
